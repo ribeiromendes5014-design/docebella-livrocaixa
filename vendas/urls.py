5b35b39aea57acfe0826dbd2371b85302e9f4e96
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-20 16:00:54 -0300

    Update urls.py

--- vendas/urls.py
+++ vendas/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lancar/', views.lancamento_vendas_view, name='vendas_lancar'),
+    # NOVO: Tela Unificada de Lançamento (Entrada ou Saída)
+    path('novo-lancamento/', views.novo_lancamento_view, name='novo_lancamento'),
+    
+    # Endpoint para consulta rápida de cliente (Ainda necessária para AJAX)
     path('buscar-cliente/', views.buscar_cliente_ajax, name='vendas_buscar_cliente_ajax'),
 ]
