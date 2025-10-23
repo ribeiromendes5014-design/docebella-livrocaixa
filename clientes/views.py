2b5e04ad20005be69b6a21ded30efa060fae0775
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-23 18:38:25 -0300

    Update views.py

--- clientes/views.py
+++ clientes/views.py
@@ -5,6 +5,9 @@ from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 from .models import Cliente
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
+
 
 # ==========================================================
 # 1. LISTAR CLIENTES (banco de dados PostgreSQL)
