24d25f56f1cde732fe477f22d9046f4984be4900
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-24 20:26:26 -0300

    Update admin.py

--- precificacao/admin.py
+++ precificacao/admin.py
@@ -1,8 +1,19 @@
+# precificacao/admin.py
 from django.contrib import admin
 from .models import Produto
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'quantidade', 'valor_pago', 'custos_extras', 'margem_lucro', 'valor_sugerido', 'criado_em')
+    list_display = (
+        'nome',
+        'quantidade',
+        'valor_pago',
+        'custo_extra',
+        'custo_total',
+        'valor_final',
+        'margem_lucro',
+        'created_at',
+    )
+    list_filter = ('created_at',)
     search_fields = ('nome',)
-    list_filter = ('criado_em',)
+    ordering = ('-created_at',)
