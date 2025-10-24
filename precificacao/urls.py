8ad812065e5182e2976543b7b36d54fe737247e0
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-24 20:50:25 -0300

    Update urls.py

--- precificacao/urls.py
+++ precificacao/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.precificacao_view, name='precificacao'),
+    path('editar/<int:pk>/', views.editar_produto, name='editar_produto'),
+    path('excluir/<int:pk>/', views.excluir_produto, name='excluir_produto'),
 ]
