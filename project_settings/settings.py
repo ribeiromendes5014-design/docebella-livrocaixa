257216b40bc7506e8ea8880bb6eccd17bcce74c6
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-23 22:44:15 -0300

    Update settings.py

--- project_settings/settings.py
+++ project_settings/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # ==============================================================================
 # ARQUIVOS ESTÁTICOS
 # ==============================================================================
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'), 
