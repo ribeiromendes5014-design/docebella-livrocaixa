185ddfc859c07ddbd57f389cd9adfe8cda7fb7e5
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-24 01:25:45 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './templates/**/*.html',
-    './**/templates/**/*.html',
-    './**/*.py',
+    './*.html',                    // base.html, base_admin.html, etc. na raiz
+    './**/*.html',                 // qualquer HTML dentro de subpastas
+    './**/templates/**/*.html',    // compatível com apps Django que tenham templates próprios
+    './**/*.py',                   // detectar classes inline em views Python
   ],
   theme: {
     extend: {},
