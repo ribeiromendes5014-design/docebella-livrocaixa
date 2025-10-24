d004d1999d06cc474d876eead7e1c6e6840954f9
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-10-24 20:13:26 -0300

    Update admin.py

--- precificacao/admin.py
+++ precificacao/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Produto
 
-# Register your models here.
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'quantidade', 'valor_pago', 'custos_extras', 'margem_lucro', 'valor_sugerido', 'criado_em')
+    search_fields = ('nome',)
+    list_filter = ('criado_em',)
